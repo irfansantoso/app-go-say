74722bb6bf72f6a4cf1dbc8ca0297200c114b23e
irfansantoso <irfan_savestheday@yahoo.com>
2021-06-26 14:02:28 +0700

    Create App Go Say

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/irfansantoso/app-go-say
+
+go 1.16
+
+require github.com/irfansantoso/go-say v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say "github.com/irfansantoso/go-say"
+)
+
+func main() {
+	fmt.Println(go_say.SayHello())
+}
